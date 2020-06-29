d591b4e66a6034e95b54954ea1d6b9b4edc24ff3
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2020-06-29 14:54:45 +0600

    Create Topological Sorting.cpp

---
+++ Graphs/Topological Sorting.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define F first
+#define S second
+#define pb push_back
+mt19937 rnd;
+
+const int N = 2e5 + 10;
+vector<int> g[N];
+bool used[N];
+vector<int> order;
+
+void dfs(int v) {
+    used[v] = 1;
+    for (int to : g[v]) {
+        if (!used[to]) {
+            dfs(to);
+        }
+    }
+    order.pb(v);
+}
+
+void topsort(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (!used[i]) dfs(i);
+    }
+    reverse(order.begin(), order.end());
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif
+
+
+
+#ifdef LOCAL
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+    return 0;
+}
