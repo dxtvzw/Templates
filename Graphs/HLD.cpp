9fce40d0cd2695b78dc0358eba19bb9502625ade
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2020-08-25 02:50:10 +0600

    Update HLD.cpp

--- Graphs/HLD.cpp
+++ Graphs/HLD.cpp
@@ -1,90 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+//typedef __int128_t LL;
+typedef long double ld;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 #define F first
 #define S second
 #define pb push_back
 mt19937 rnd;
-/*
-    RANGE UPDATES
+
+const int N = 5e5 + 5;
+const int L = 21;
+const int inf = 1e9 + 10;
 
 struct segment_tree {
-    int N;
-    vector<int> t;
-    segment_tree(int n) {
-        N = n;
-        t.resize(2 * N + 5, 0);
-    }
-    void clear() {
-        for (int& i : t) {
-            i = 0;
+    int t[2 * N + 5];
+    void build() {
+        for (int i = N - 1; i > 0; i--) {
+            t[i] = min(t[i << 1], t[i << 1 | 1]);
         }
-    }
-    void update(int l, int r, int val) {
-        for (l += N, r += N; l <= r; l >>= 1, r >>= 1) {
-            if (l & 1) t[l++] += val;
-            if (!(r & 1)) t[r--] += val;
-        }
-    }
-    int get(int pos) {
-        ll res = 0;
-        for (pos += N; pos > 0; pos >>= 1) {
-            res += t[pos];
-        }
-        return res;
-    }
-};
-*/
-
-struct segment_tree {
-    int N;
-    vector<int> t;
-    segment_tree(int n) {
-        N = n;
-        t.resize(2 * N + 5);
     }
     void update(int pos, int val) {
-        for (t[pos += N] += val; pos > 1; pos >>= 1) {
-            t[pos >> 1] = t[pos] + t[pos ^ 1];
+        for (t[pos += N] = val; pos > 1; pos >>= 1) {
+            t[pos >> 1] = min(t[pos], t[pos ^ 1]);
         }
     }
     int get(int l, int r) {
-        int res = 0;
+        int res = inf;
         for (l += N, r += N; l <= r; l >>= 1, r >>= 1) {
-            if (l & 1) res += t[l++];
-            if (!(r & 1)) res += t[r--];
+            if (l & 1) res = min(res, t[l++]);
+            if (!(r & 1)) res = min(res, t[r--]);
         }
         return res;
     }
-};
+} tree;
 
-struct heavy_path {
-    vector<int> path;
-    segment_tree t;
-};
+/*
+    down[v] - child of v in its heavy path
+    start[i] - array of starting positions of each heavy path
+    st_prt - pointer to the first available cell in start[i]
+    path_st[v] - highest vertex on v's heavy path
+    pos[v] - position of v in the segment tree
 
-const int N = 1e5 + 10;
-const int L = 17;
+    values are contained in vertexes
+*/
+
+int timer;
 vector<int> g[N];
 int in[N], out[N];
-int up[N][L];
-int timer;
-int depth[N];
+int up[N][L], down[N];
+int sz[N], depth[N];
+int start[N], st_ptr;
+int path_st[N], pos[N];
 
-void dfs_lca(int v = 1, int p = 1) {
-	in[v] = ++timer;
+bool is_heavy(int v, int to) {
+    return sz[to] >= (sz[v] + 1) / 2;
+}
+
+void dfs_hld(int v = 1, int p = 1) {
+    in[v] = ++timer;
 	up[v][0] = p;
 	for (int i = 1; i < L; i++) {
-		up[v][i] = up[up[v][i-1]][i-1];
+		up[v][i] = up[up[v][i - 1]][i - 1];
 	}
-	for (int to : g[v]) {
-		if (to != p) {
+    sz[v] = 1;
+    for (int to : g[v]) {
+        if (to != p) {
             depth[to] = depth[v] + 1;
-            dfs_lca(to, v);
-		}
-	}
-	out[v] = ++timer;
+            dfs_hld(to, v);
+            sz[v] += sz[to];
+        }
+    }
+    bool light = 1;
+    for (int to : g[v]) {
+        if (to != p && is_heavy(v, to)) {
+            light = 0;
+            break;
+        }
+    }
+    if (light) {
+        start[st_ptr++] = v;
+    }
+    out[v] = ++timer;
 }
 
 bool is_anc(int u, int v) {
@@ -104,83 +102,35 @@ int dist(int u, int v) {
     return depth[u] + depth[v] - 2 * depth[lca(u, v)];
 }
 
-int sz[N];
-vector<int> start;
-vector<heavy_path> hld;
-int id[N], pos[N];
-
-bool heavy(int v, int to) {
-    return sz[to] >= (sz[v] + 1) / 2;
-}
-
-void dfs_hld(int v = 1, int p = 1) {
-    sz[v] = 1;
-    for (int to : g[v]) {
-        if (to != p) {
-            dfs_hld(to, v);
-            sz[v] += sz[to];
-        }
-    }
-    bool light = 1;
-    for (int to : g[v]) {
-        if (to != p && heavy(v, to)) {
-            light = 0;
-            break;
-        }
-    }
-    if (light) start.pb(v);
-}
-
-void heavy_light_decomposition(int root = 1) {
-    dfs_lca(root, root);
+void heavy_light_decomposition(int n, int root = 1) {
     dfs_hld(root, root);
-    for (int i : start) {
-        int v = i;
-        vector<int> path = {v};
-        while (v != root && heavy(up[v][0], v)) {
+    for (int i = 0, ptr = 0; i < st_ptr; i++) {
+        int v = start[i];
+        while (v != root && is_heavy(up[v][0], v)) {
+            down[up[v][0]] = v;
             v = up[v][0];
-            path.pb(v);
         }
-        reverse(path.begin(), path.end());
-        segment_tree t(path.size());
-        hld.pb({path, t});
-    }
-    for (int i = 0; i < hld.size(); i++) {
-        for (int j = 0; j < hld[i].path.size(); j++) {
-            id[hld[i].path[j]] = i;
-            pos[hld[i].path[j]] = j;
+        int cur_st = v;
+        while (v) {
+            path_st[v] = cur_st;
+            pos[v] = ptr++;
+            v = down[v];
         }
     }
 }
-
-/*
-    RANGE UPDATES
-
-void update(int u, int v, int val) {
-    while (id[v] != id[u]) {
-        hld[id[v]].t.update(0, pos[v], val);
-        v = par[hld[id[v]].path[0]];
-    }
-    hld[id[v]].t.update(pos[u] + 1, pos[v], val);
-}
-
-int get(int v) {
-    return hld[id[v]].t.get(pos[v]);
-}
-*/
 
 void update(int v, int val) {
-    hld[id[v]].t.update(pos[v], val);
+    tree.update(pos[v], val);
 }
 
 int get(int u, int v) {
     if (is_anc(u, v)) {
-        int res = 0;
-        while (id[v] != id[u]) {
-            res = max(res, hld[id[v]].t.get(0, pos[v]));
-            v = up[hld[id[v]].path[0]][0];
+        int res = inf;
+        while (path_st[v] != path_st[u]) {
+            res = min(res, tree.get(pos[path_st[v]], pos[v]));
+            v = up[path_st[v]][0];
         }
-        res = max(res, hld[id[v]].t.get(pos[u], pos[v]));
+        res = min(res, tree.get(pos[u], pos[v]));
         return res;
     }
     else if (is_anc(v, u)) {
@@ -188,7 +138,7 @@ int get(int u, int v) {
     }
     else {
         int l = lca(u, v);
-        return max(get(l, u), get(l, v));
+        return min(get(l, u), get(l, v));
     }
 }
 
@@ -198,7 +148,7 @@ int main() {
     freopen("input.txt", "r", stdin);
 #endif
 
-    heavy_light_decomposition();
+
 
 #ifdef LOCAL
     cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
