e9ae9489c93ae2ead5adef20a9f078e48b5928e4
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2020-09-18 15:44:39 +0600

    Update Li Chao Tree on Pointers.cpp

--- Dynamic Programming/CHT/Li Chao Tree on Pointers.cpp
+++ Dynamic Programming/CHT/Li Chao Tree on Pointers.cpp
@@ -27,48 +27,48 @@ struct Node {
 };
 
 ll dot(point a, point b) {
-	return (conj(a) * b).x();
+    return (conj(a) * b).x();
 }
 
 ll f(point a, ll x) {
-	return dot(a, {x, 1});
+    return dot(a, {x, 1});
 }
 
 point line[4*N];
 Node *root[N];
 
 void build() {
-	for (int i=0; i<4*N; i++) line[i] = {0, inf};
+    for (int i=0; i<4*N; i++) line[i] = {0, inf};
 }
 
 void add_line(point nw, Node *v) {
-	if (f(nw, v->l) >= f(v->line, v->l) && f(nw, v->r) >= f(v->line, v->r)) return;
-	int m = (v->l + v->r) / 2;
-	bool lef = f(nw, v->l) < f(v->line, v->l);
-	bool mid = f(nw, m) < f(v->line, m);
-	if (mid) swap(v->line, nw);
-	if (v->l + 1 == v->r) return;
-	else if (lef != mid) {
-		if (v->lef == NULL) v->lef = new Node(v->l, m);
-		add_line(nw, v->lef);
-	}
-	else {
-		if (v->rig == NULL) v->rig = new Node(m, v->r);
-		add_line(nw, v->rig);
-	}
+    if (f(nw, v->l) >= f(v->line, v->l) && f(nw, v->r) >= f(v->line, v->r)) return;
+    int m = (v->l + v->r) / 2;
+    bool lef = f(nw, v->l) < f(v->line, v->l);
+    bool mid = f(nw, m) < f(v->line, m);
+    if (mid) swap(v->line, nw);
+    if (v->l + 1 == v->r) return;
+    else if (lef != mid) {
+        if (v->lef == NULL) v->lef = new Node(v->l, m);
+        add_line(nw, v->lef);
+    }
+    else {
+        if (v->rig == NULL) v->rig = new Node(m, v->r);
+        add_line(nw, v->rig);
+    }
 }
 
 ll get(int x, Node *v) {
-	int m = (v->l + v->r) / 2;
-	if (v->l + 1 == v->r) return f(v->line, x);
-	else if (x < m) {
-		if (v->lef == NULL) return f(v->line, x);
-		else return min(f(v->line, x), get(x, v->lef));
-	}
-	else {
-		if (v->rig == NULL) return f(v->line, x);
-		else return min(f(v->line, x), get(x, v->rig));
-	}
+    int m = (v->l + v->r) / 2;
+    if (v->l + 1 == v->r) return f(v->line, x);
+    else if (x < m) {
+        if (v->lef == NULL) return f(v->line, x);
+        else return min(f(v->line, x), get(x, v->lef));
+    }
+    else {
+        if (v->rig == NULL) return f(v->line, x);
+        else return min(f(v->line, x), get(x, v->rig));
+    }
 }
 
 void merge(Node *v, Node *u) {
