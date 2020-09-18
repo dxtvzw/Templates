1419398bfde4bdb58e6e5eb34c820fa3f7a68ad8
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2020-09-18 15:43:19 +0600

    Tabs

--- Dynamic Programming/CHT/CHT.cpp
+++ Dynamic Programming/CHT/CHT.cpp
@@ -8,29 +8,29 @@ typedef long long ll;
 mt19937 rnd;
 
 struct CHT {
-	int pointer=0;
-	vector<ll> K, B;
-	void clear() {
-		pointer = 0;
-		K.clear();
-		B.clear();
-	}
-	bool bad(int l1, int l2, int l3) {
-		return (B[l3]-B[l1])*(K[l1]-K[l2])<(B[l2]-B[l1])*(K[l1]-K[l3]);
-	}
-	void add(ll k, ll b) {
-		if (!K.empty() && K.back() == k) return;
-		K.pb(k);
-		B.pb(b);
-		while (K.size() >= 3 && bad(K.size()-3, K.size()-2, K.size()-1)) {
-			K.erase(K.end()-2);
-			B.erase(B.end()-2);
-		}
-	}
-	ll getmin(ll x) {
-		while (pointer < K.size()-1 && K[pointer+1] * x + B[pointer+1] < K[pointer] * x + B[pointer]) pointer++;
-		return K[pointer] * x + B[pointer];
-	}
+    int pointer=0;
+    vector<ll> K, B;
+    void clear() {
+        pointer = 0;
+        K.clear();
+        B.clear();
+    }
+    bool bad(int l1, int l2, int l3) {
+        return (B[l3]-B[l1])*(K[l1]-K[l2])<(B[l2]-B[l1])*(K[l1]-K[l3]);
+    }
+    void add(ll k, ll b) {
+        if (!K.empty() && K.back() == k) return;
+        K.pb(k);
+        B.pb(b);
+        while (K.size() >= 3 && bad(K.size()-3, K.size()-2, K.size()-1)) {
+            K.erase(K.end()-2);
+            B.erase(B.end()-2);
+        }
+    }
+    ll getmin(ll x) {
+        while (pointer < K.size()-1 && K[pointer+1] * x + B[pointer+1] < K[pointer] * x + B[pointer]) pointer++;
+        return K[pointer] * x + B[pointer];
+    }
 };
 
 int main() {
