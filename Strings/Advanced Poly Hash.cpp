9c9d578bf5c677bff0164d034aef152fa5896802
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2020-09-18 16:02:51 +0600

    Update Advanced Poly Hash.cpp

--- Strings/Advanced Poly Hash.cpp
+++ Strings/Advanced Poly Hash.cpp
@@ -7,58 +7,75 @@ typedef pair<int, int> pii;
 #define pb push_back
 mt19937 rnd;
 
-template <int mod>
+template<int mod>
 struct Modular {
-	int val;
-	Modular(int new_val = 0) {
-	    val = new_val;
-	}
-	Modular operator +(const Modular& ot) {
-		if (val + ot.val >= mod) return val + ot.val - mod;
-		else return val + ot.val;
-	}
-	Modular operator -(const Modular& ot) {
-		if (val - ot.val < 0) return val - ot.val + mod;
-		else return val - ot.val;
-	}
-	Modular operator *(const Modular& ot) {
-		return 1ll * val * ot.val % mod;
-	}
-	Modular& operator ++() {
+    int val;
+    Modular(int new_val = 0) {
+        val = new_val;
+    }
+    Modular operator+(const Modular& ot) const {
+        if (val + ot.val >= mod) return val + ot.val - mod;
+        else return val + ot.val;
+    }
+    Modular operator-(const Modular& ot) const {
+        if (val - ot.val < 0) return val - ot.val + mod;
+        else return val - ot.val;
+    }
+    Modular operator*(const Modular& ot) const {
+        return 1ll * val * ot.val % mod;
+    }
+    friend Modular binpow(Modular a, ll n) {
+        Modular res = 1;
+        for (; n; n >>= 1) {
+            if (n & 1) res *= a;
+            a *= a;
+        }
+        return res;
+    }
+    friend Modular inv(const Modular& a) {
+        return binpow(a, mod - 2);
+    }
+    Modular operator/(const Modular& ot) const {
+        return 1ll * val * inv(ot).val % mod;
+    }
+    Modular& operator++() {
         if (val + 1 == mod) return 0;
         else return val + 1;
-	}
-	Modular operator ++(int) {
-		Modular tmp(*this);
-		operator ++();
-		return tmp;
-	}
-	void operator +=(const Modular& ot) {
-		val += ot.val;
-		if (val >= mod) {
-			val -= mod;
-		}
-	}
-	void operator -=(const Modular& ot) {
-		val -= ot.val;
-		if (val < 0) {
-			val += mod;
-		}
-	}
-	void operator *=(const Modular& ot) {
-		val = 1ll * val * ot.val % mod;
-	}
-	bool operator ==(const Modular& ot) {
-		return val == ot.val;
-	}
-	bool operator !=(const Modular& ot) {
-		return val != ot.val;
-	}
+    }
+    void operator+=(const Modular& ot) {
+        val += ot.val;
+        if (val >= mod) {
+            val -= mod;
+        }
+    }
+    void operator-=(const Modular& ot) {
+        val -= ot.val;
+        if (val < 0) {
+            val += mod;
+        }
+    }
+    void operator*=(const Modular& ot) {
+        val = 1ll * val * ot.val % mod;
+    }
+    void operator/=(const Modular& ot) {
+        val = 1ll * val * inv(ot).val % mod;
+    }
+    bool operator==(const Modular& ot) const {
+        return val == ot.val;
+    }
+    bool operator!=(const Modular& ot) const {
+        return val != ot.val;
+    }
 };
 
-template <int mod>
-ostream& operator <<(ostream& ostr, const Modular<mod>& x) {
-	return ostr << x.val;
+template<int mod>
+istream& operator>>(istream& istr, Modular<mod>& x) {
+    return istr >> x.val;
+}
+
+template<int mod>
+ostream& operator<<(ostream& ostr, const Modular<mod>& x) {
+    return ostr << x.val;
 }
 
 template <int mod>
