b6db55c7ae71a4b41a045b9c7ddec465479fa0b1
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2020-09-18 15:55:25 +0600

    Update Max Matching - Edmond's Blossom Algorithm.cpp

--- Graphs/Max Matching - Edmond's Blossom Algorithm.cpp
+++ Graphs/Max Matching - Edmond's Blossom Algorithm.cpp
@@ -17,83 +17,83 @@ bool used_lca[N];
 queue<int> q;
 
 int lca(int a, int b) {
-	for (int i = 0; i <= N - 1; i++) {
+    for (int i = 0; i <= N - 1; i++) {
         used_lca[i] = 0;
-	}
-	while (1) {
+    }
+    while (1) {
         a = base[a];
         used_lca[a] = 1;
         if (!match[a]) break;
         a = parent[match[a]];
-	}
-	while (1) {
+    }
+    while (1) {
         b = base[b];
         if (used_lca[b]) return b;
         b = parent[match[b]];
-	}
+    }
 }
 
 void mark_path(int v, int b, int child) {
-	while (base[v] != b) {
-		blossom[base[v]] = blossom[base[match[v]]] = 1;
-		parent[v] = child;
-		child = match[v];
-		v = parent[match[v]];
-	}
+    while (base[v] != b) {
+        blossom[base[v]] = blossom[base[match[v]]] = 1;
+        parent[v] = child;
+        child = match[v];
+        v = parent[match[v]];
+    }
 }
 
 int find_path(int root, int n) {
     for (int i = 1; i <= n; i++) {
-		used[i] = parent[i] = 0;
-		base[i] = i;
-	}
-	used[root] = 1;
-	while (!q.empty()) q.pop();
-	q.push(root);
-	while (!q.empty()) {
-		int v = q.front();
-		q.pop();
-		for (int to : g[v]) {
-			if (base[v] == base[to] || match[v] == to)  continue;
-			if (to == root || match[to] && parent[match[to]]) {
-				int cur_base = lca (v, to);
-				for (int i=1; i<=n; i++) blossom[i] = 0;
-				mark_path(v, cur_base, to);
-				mark_path(to, cur_base, v);
-				for (int i=1; i<=n; i++) {
-					if (blossom[base[i]]) {
-						base[i] = cur_base;
-						if (!used[i]) {
-							used[i] = 1;
-							q.push(i);
-						}
-					}
-				}
-			}
-			else if (!parent[to]) {
-				parent[to] = v;
-				if (!match[to]) return to;
-				to = match[to];
-				used[to] = 1;
-				q.push(to);
-			}
-		}
-	}
-	return 0;
+        used[i] = parent[i] = 0;
+        base[i] = i;
+    }
+    used[root] = 1;
+    while (!q.empty()) q.pop();
+    q.push(root);
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (int to : g[v]) {
+        if (base[v] == base[to] || match[v] == to)  continue;
+            if (to == root || match[to] && parent[match[to]]) {
+            int cur_base = lca (v, to);
+                for (int i=1; i<=n; i++) blossom[i] = 0;
+                mark_path(v, cur_base, to);
+                mark_path(to, cur_base, v);
+                for (int i=1; i<=n; i++) {
+                    if (blossom[base[i]]) {
+                        base[i] = cur_base;
+                        if (!used[i]) {
+                            used[i] = 1;
+                            q.push(i);
+                        }
+                    }
+                }
+            }
+            else if (!parent[to]) {
+                parent[to] = v;
+                if (!match[to]) return to;
+                to = match[to];
+                used[to] = 1;
+                q.push(to);
+            }
+        }
+    }
+    return 0;
 }
 
 void Edmonds(int n) {
-	for (int i = 1; i <= n; i++) {
-		if (!match[i]) {
-			int v = find_path(i, n);
-			while (v) {
-				int match_par_v = match[parent[v]];
-				match[v] = parent[v];
-				match[parent[v]] = v;
-				v = match_par_v;
-			}
-		}
-	}
+    for (int i = 1; i <= n; i++) {
+        if (!match[i]) {
+            int v = find_path(i, n);
+            while (v) {
+                int match_par_v = match[parent[v]];
+                match[v] = parent[v];
+                match[parent[v]] = v;
+                v = match_par_v;
+            }
+        }
+    }
 }
 
 int main() {
