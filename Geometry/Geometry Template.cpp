f6243360a5a6d4cd31416c7d920a5094d6f1c573
Alikhan Zimanov <46425731+ALILILILILI-KHAN@users.noreply.github.com>
2021-01-13 13:18:10 +0300

    Update Geometry Template.cpp

--- Geometry/Geometry Template.cpp
+++ Geometry/Geometry Template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-//typedef long long ll;
-typedef __int128_t ll;
+typedef long long ll;
+typedef __int128_t LL;
 typedef long double ld;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
